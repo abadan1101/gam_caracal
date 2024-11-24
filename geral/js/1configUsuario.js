29ba557921544065bcd012d95c64de34b6a84172
Abadan <abadankaristad@gmail.com>
2024-11-24 06:51:46 -0300

    configurações

--- geral/js/1configUsuario.js
+++ geral/js/1configUsuario.js
@@ -1,8 +1,17 @@
 //---------------------CONFIGURAÇÕES DO USUÁRIO---------------------------
 //-----------------------------------------------------------------------
 
+//menu responsivo: 1500, recolhido: 5000 ou menu fixo: 800
+if(!localStorage.getItem("tipoMenu")){localStorage.setItem("tipoMenu", 5000)}
+
+//estatica: não é possivel editar a tabela sem o formulário; normal: tabela editavel.
+if(!localStorage.getItem("tipoTabela")){localStorage.setItem("tipoTabela", "normal")}
+
 //variavel do tamanho da tela padrão em px
-const tipMenu = 5000; //se menu responsivo: 1500, recolhido: 5000 ou menu fixo: 800
+var tipMenu = localStorage.getItem("tipoMenu"); 
 
 //variavel que difine tipo de tabela principal
-const tblPrincipal = "normal" //estatica: não é possivel editar a tabela sem o formulário; normal: tabela editavel.
+var tblPrincipal = localStorage.getItem("tipoTabela") 
+
+localStorage.setItem("tipoMenu", 800)
+localStorage.setItem("tipoTabela", "estatica")
