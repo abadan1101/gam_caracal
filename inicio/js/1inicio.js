ee7f1796209b07e1721d6ba43c4e839ecde9f5ce
abadan1101 <abadankaristad@gmail.com>
2025-04-08 14:44:01 -0300

    Add files via upload

--- inicio/js/1inicio.js
+++ inicio/js/1inicio.js
@@ -3,36 +3,74 @@
 async function graficosLinha01(){
 
     //verificar se está ativo
+    const dbConfig = await loadTBCfgLin(0)
     const db = await loadTBCfgLin(1)
 
     if(db.status == "inativo"){
         document.getElementById('myChartLinha01').style.display = "none"
     }else{
-
+	
+	const dbData =  [78, 50, 100, 50, 30, 57, 98, 42, 65, 23];
+	
+	document.getElementById("myChartANV").innerHTML = "LINHA 01 ANV " + db.nANV
+	
         document.getElementById('myChartLinha01').style.display = "block"
         //ANDAMENTO DOS CARTÕES
         const ctx = document.getElementById('myChart');
         const data = {
             labels: [
-                'Red',
-                'Blue',
-                'Yellow'
+                dbConfig.chave00[2],
+                dbConfig.chave00[3],
+                dbConfig.chave00[4],
+                dbConfig.chave00[5],
+                dbConfig.chave00[6],
+                dbConfig.chave00[7],
+                dbConfig.chave00[8],
+                dbConfig.chave00[9],
+                dbConfig.chave00[10],
+                dbConfig.chave00[11],
             ],
             datasets: [{
-                label: 'My First Dataset',
-                data: [300, 50, 100],
+                label: 'Cartões de inspeção',
+                data: dbData,
                 backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
+		        '#f77',
+		        '#2E8B57',
+		        '#FFD700',
+		        '#9fcd9f',
+		        '#fff',
+		        '#e2e1e1',
+		        '#599cf3',
+		        '#a175f8',
+		        '#F00',
+		        '#BC8F8F'
                 ],
-                hoverOffset: 4
+                borderColor: [
+                 	'#f77',
+		        '#2E8B57',
+		        '#FFD700',
+		        '#9fcd9f',
+		        '#ccc',
+		        '#e2e1e1',
+		        '#599cf3',
+		        '#a175f8',
+		        '#F00',
+		        '#BC8F8F'
+                ],
+		borderWidth: 1,
+		barPercentage: 0.5,
+		barThickness: 20,
+		maxBarThickness: 20,
+		minBarLength: 2,
             }]
         };
 
         new Chart(ctx, {
-            type: 'doughnut',
+            type: 'bar',
             data: data,
+            options: {
+	    	responsive: true
+	    },
         });
 
         //ANDAMENTO DAS INSPEÇÕES
